eba613b5415c0b8acdc38f98076a61384ce98f53
JellaShyam <shyamjella5089@gmail.com>
2018-02-13 22:40:27 +0530

    Initial commit

---
+++ Movies/Controllers/MoviesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Movies.Controllers
+{
+    public class MoviesController : Controller
+    {
+        // GET: Movies
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Create()
+        {
+            return View();
+        }
+        public ActionResult WatchMovie()
+        {
+            return View();
+        }
+    }
+}
